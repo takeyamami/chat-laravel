7c8ca487ebb461cac2648e178a539503b1aec3d9
takeyamami <task8705@yahoo.co.jp>
2021-05-29 23:03:20 +0900

    add: validatorの作成

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\LoginRequest;
+use Validator;
 
 class LoginController extends Controller
 {
@@ -13,12 +13,33 @@ class LoginController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request) {
+    public function __invoke(Request $request) 
+    {
         return view('login.index', ['data' => $request->data]);
     }
 
-    public function action(LoginRequest $request) {
+    public function action(Request $request) 
+    {
+        $rules = [
+            'email' => 'required|email',
+            'password' => 'required|digits_between:8,20'
+        ];
 
-        return view('login.index', ['data' => $request->data]);
+        $messages = [
+            'email.required' => 'メールアドレスは必須です。',
+            'email.email' => 'メールアドレスの形式が正しくありません。',
+            'password.required' => 'パスワードは必須です。',
+            'password.digits_between' => 'パスワードは8文字以上、20文字以下で入力してください。'
+        ];
+    
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if($validator->fails()) {
+            return redirect('/login')
+            ->withErrors($validator)
+            ->withInput();
+        }
+
+        return redirect('/chat');
     }
 }
