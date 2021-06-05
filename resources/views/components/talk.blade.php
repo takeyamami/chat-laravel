2ff0d038482ad04236cf711e4a58d4d9f5d5be46
takeyamami <task8705@yahoo.co.jp>
2021-06-05 22:06:08 +0900

    update: 自分のメッセージと相手のメッセージに区別がつくように修正

--- resources/views/components/talk.blade.php
+++ resources/views/components/talk.blade.php
@@ -3,9 +3,9 @@
 $talkPosition = $val->type == 1 ? 'talk-me' : 'talk-other';
 @endphp
 
-<div class="talk">
+<div class="talk {{$talkPosition}}">
 <div class="name">{{$val->username}}</div>
-    <div class="{{$talkPosition}}">
+    <div class="message">
         <p>{{$val->message}}</p>
     </div>
 <div class="time">{{$val->regist_dt}}</div>
