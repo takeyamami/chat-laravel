12035ead5fb681292297dc96b34fe5de1565bc78
takeyamami <task8705@yahoo.co.jp>
2021-06-05 22:27:06 +0900

    update: 改行が出力されるように修正

--- resources/views/components/talk.blade.php
+++ resources/views/components/talk.blade.php
@@ -6,7 +6,7 @@ $talkPosition = $val->type == 1 ? 'talk-me' : 'talk-other';
 <div class="talk {{$talkPosition}}">
 <div class="name">{{$val->username}}</div>
     <div class="message">
-        <p>{{$val->message}}</p>
+        <p>{!! nl2br(e($val->message)) !!}</p>
     </div>
 <div class="time">{{$val->regist_dt}}</div>
 </div>
