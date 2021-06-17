9bf340b66d6c5e47fd20b2db1f09b3d5eba02193
takeyamami <task8705@yahoo.co.jp>
2021-06-17 20:40:10 +0900

    bugfix: Talkモデルのパスが誤っていたため修正

--- package-web/Http/Middleware/PostMiddleware.php
+++ package-web/Http/Middleware/PostMiddleware.php
@@ -5,7 +5,7 @@ namespace MyApp\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Models\Talk;
+use MyApp\Models\Talk;
 
 class PostMiddleware
 {
